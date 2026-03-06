2d165b40cef8b8e06febc5e25354cfbb0daedb17
embeddedJedii <ridwan.innov8@gmail.com>
2026-03-06 15:56:41 +0100

    Adding a 3rd pzem and begining with the shutdown logic

--- Smart_load_management_system/src/main.cpp
+++ Smart_load_management_system/src/main.cpp
@@ -5,7 +5,8 @@
 //Setting up the PZEM hardwareSerial 
 HardwareSerial pzemSerial(2); // Use UART1 for PZEM communication
 PZEM004Tv30 pzem1(pzemSerial, 16, 17, 0x01); // Initialize PZEM with the hardware serial
-PZEM004Tv30 pzem2(pzemSerial, 16, 17, 0x55); // Initialize second PZEM with the same hardware serial but different pins and address
+PZEM004Tv30 pzem2(pzemSerial, 16, 17, 0x44); // Initialize second PZEM with the same hardware serial but different pins and address
+PZEM004Tv30 pzem3(pzemSerial, 16, 17, 0x55); // Initialize third PZEM with the same hardware serial but different pins and address
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,7 +18,8 @@ void setup() {
 void loop() {
 // put your main code here, to run repeatedly:
 readPZEM(pzem1, "The address for this pzem is 0x01");
-readPZEM(pzem2, "The address for this PZEM is 0x55");
+readPZEM(pzem2, "The address for this PZEM is 0x44");
+readPZEM(pzem3, "The address for this PZEM is 0x55");
 Serial.println("==============================");
 delay(2000);
 }
--- Smart_load_management_system/src/pzem.cpp
+++ Smart_load_management_system/src/pzem.cpp
@@ -1,22 +1,6 @@
 
 #include "pzem.h"
 #include <PZEM004Tv30.h>
-// void readPzem(PZEM004Tv30 &pzem, String name) {
-//   float voltage = pzem.voltage();
-//   float current = pzem.current();
-//   float power = pzem.power();
-//   float energy = pzem.energy();
-//   uint16_t frequency = pzem.frequency();
-// //   uint8_t pf = pzem.pf();
-
-//   Serial.printf("Voltage: %.2f V\n", voltage);
-//   Serial.printf("Current: %.2f A\n", current);
-//   Serial.printf("Power: %.2f W\n", power);
-//   Serial.printf("Energy: %.2f Wh\n", energy);
-//   Serial.printf("Frequency: %d Hz\n", frequency);
-// //   Serial.printf("Power Factor: %d%%\n", pf);
-// }
-
 void readPZEM(PZEM004Tv30 &pzem, String name)
 {
     float voltage = pzem.voltage();
@@ -32,4 +16,32 @@ void readPZEM(PZEM004Tv30 &pzem, String name)
     Serial.printf("Power: %.2f W\n", power);
     Serial.printf("Energy: %.2f Wh\n", energy);
     Serial.printf("Frequency: %.2f Hz\n", frequency);
+}
+
+void HighestPower(PZEM004Tv30 &pzem1, PZEM004Tv30 &pzem2, PZEM004Tv30 &pzem3, int batteryPercentage)
+{
+    float power1 = pzem1.power();
+    float power2 = pzem2.power();
+    float power3 = pzem3.power();
+
+    if(batteryPercentage <= 70 && batteryPercentage > 45) {
+        if (power1 > power2 && power1 > power3) {
+        Serial.println("PZEM 1 has the highest power consumption.");
+        //disconnect the load connected to the pzem with power1
+    } else if (power2 > power1 && power2 > power3) {
+        Serial.println("PZEM 2 has the highest power consumption.");
+    } else if (power3 > power1 && power3 > power2) {
+        Serial.println("PZEM 3 has the highest power consumption.");
+    } else {
+        Serial.println("Two or more PZEMs have the same highest power consumption.");
+    }
+    }
+    
+    if(batteryPercentage<= 45 && batteryPercentage > 20){
+        //turn off the load with the next highest power
+    }
+
+    if(batteryPercentage <= 20){
+        //turn off all loads
+    }
 }
